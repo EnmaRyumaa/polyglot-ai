cb59dee259f864d1410c0e6b4f97cdcc82e9f4ba
EnmaRyumaa <fer.augustofs@gmail.com>
2025-08-07 16:33:49 -0300

    Start controller for translate

--- src/main/java/com/polyglotai/controllers/PolyglotaiController.java
+++ src/main/java/com/polyglotai/controllers/PolyglotaiController.java
@@ -1,7 +1,16 @@
 package com.polyglotai.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/v1")
 public class PolyglotaiController {
+
+    @PostMapping(value = "/translate")
+    public ResponseEntity<String> getTranslate () {
+        return ResponseEntity.ok(null);
+    }
 }
