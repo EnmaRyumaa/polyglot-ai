e1c56cddba47bdff24b7a8ae177bf4b8f347c164
EnmaRyumaa <fer.augustofs@gmail.com>
2025-08-07 16:09:35 -0300

    Initial commit

---
+++ src/main/java/com/polyglotai/PolyglotaiApplication.java
@@ -0,0 +1,13 @@
+package com.polyglotai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyglotaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyglotaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polyglotai/PolyglotaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polyglotai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyglotaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
