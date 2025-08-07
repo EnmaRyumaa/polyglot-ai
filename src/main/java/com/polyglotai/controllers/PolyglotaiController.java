dea8af8faa75d9e3badc3fab2eaabb55b797feb7
EnmaRyumaa <fer.augustofs@gmail.com>
2025-08-07 16:37:36 -0300

    Prepare controller for receive a json

--- src/main/java/com/polyglotai/controllers/PolyglotaiController.java
+++ src/main/java/com/polyglotai/controllers/PolyglotaiController.java
@@ -2,6 +2,7 @@ package com.polyglotai.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PolyglotaiController {
 
     @PostMapping(value = "/translate")
-    public ResponseEntity<String> getTranslate () {
-        return ResponseEntity.ok(null);
+    public ResponseEntity<String> getTranslate (@RequestBody String body) {
+        return ResponseEntity.ok(body);
     }
 }
